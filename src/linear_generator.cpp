8200ff24771f3df9ad85a9a647cbe8300d5ca1cb
Diptodip Deb <dr.diptodip@gmail.com>
2016-06-28 11:03:59 -0500

    Build file progressively.

--- src/linear_generator.cpp
+++ src/linear_generator.cpp
@@ -42,16 +42,18 @@ int main(int argc, char** argv) {
     }
     in.close();
 
+    /*
+
     const int bothsquared = pm_size * pm_size * vm_size * vm_size;
     const int sum = bothsquared + (pm_size * vm_size);
 
     vector<string> assign_layer(vm_size);
     vector<vector<string> > assignments(pm_size, assign_layer);
-    vector<string> bounds(sum);
-    vector<string> variable_matching(bothsquared);
-    vector<string> assignment_guarantee(vm_size);
-    vector<string> capacity_constraint(pm_size);
-    vector<string> obj(bothsquared);
+    string bounds(sum);
+    string variable_matching(bothsquared);
+    string assignment_guarantee(vm_size);
+    string capacity_constraint(pm_size);
+    string obj(bothsquared);
 
     cout << "[out] writing objective and generating variable matching constraints and variable bounds" << endl;
 
@@ -59,11 +61,11 @@ int main(int argc, char** argv) {
     int bounds_counter = 0;
     for(int u = 0; u < pm_size; u++) {
         for (int i = 0; i < vm_size; i++) {
-            string varname = "a_" + to_string(acounter);
+            string varname += "a_" + to_string(acounter);
             assignments[u][i] = varname;
             acounter++;
-            bounds[bounds_counter] = varname;
-            bounds_counter++;
+            varname += " <= 1\n";
+            bounds += "0 <=" + varname;
         }
     }
     
@@ -81,8 +83,8 @@ int main(int argc, char** argv) {
                     bounds[bounds_counter] = varname;
                     bounds_counter++;
                     string matchvar;
-                    matchvar = "-" + varname + " + " + assignments[u][i] + " + " + assignments[v][j];
-                    variable_matching[vmatch_counter] = matchvar;
+                    matchvar += "-" + varname + " + " + assignments[u][i] + " + " + assignments[v][j];
+                    variable_matching += matchvar;
                     vmatch_counter++;
                     counter++;
                     int distance = distances[u][v];
@@ -91,7 +93,7 @@ int main(int argc, char** argv) {
                     if (demand > -1 && distance > -1) {
                         total = distance * demand;
                     }
-                    string objterm = to_string(total) + " " + varname;
+                    string objterm += to_string(total) + " " + varname;
                     obj[objcounter] = objterm;
                     objcounter++;
                 }
@@ -162,6 +164,96 @@ int main(int argc, char** argv) {
     out << "Bounds" << endl;
     for (int i = 0; i < bounds.size(); i++) {
         out << "0 <= " << bounds[0] << " <= 1" << endl;
+    }
+    out << "End" << endl;
+    out.close();
+    */
+
+    ofstream out(fileout);
+    out << "Minimize" << endl;
+    out << "obj: ";
+    int objcounter = 0;
+    cout << "[out] writing objective" << endl;
+    for (int u = 0; u < pm_size; u++) {
+        for (int v = 0; v < pm_size; v++) {
+            for (int i = 0; i < vm_size; i++) {
+                for (int j = 0; j < vm_size; j++) {
+                    int total = 0;
+                    int distance = distances[u][v];
+                    int demand = traffic[i][j];
+                    if (demand > -1 && distance > -1) {
+                        total = distance * demand;
+                    }
+                    if (objcounter == 0) {
+                        out << total << " " << "o_" << objcounter;
+                    } else {
+                        out << " + " << total << " " << "o_" << objcounter;
+                    }
+                }
+            }
+        }
+    }
+    out << endl;
+    out << "Subject To" << endl;
+    int match_counter = 0;
+    int counter = 0;
+    for (int u = 0; u < pm_size; u++) {
+        for (int v = 0; v < pm_size; v++) {
+            for (int i = 0; i < vm_size; i++) {
+                for (int j = 0; j < vm_size; j++) {
+                    out << "c" << counter << ": " << "-" << "o_" << match_counter << " + " << "a_" << u << "_" << i << " + " << "a_" << v << "_" << j << " <= 1" << endl;
+                    counter++;
+                    match_counter++;
+                }
+            }
+        }
+    }
+    int capacity_counter = 0;
+    for (int u = 0; u < pm_size; u++) {
+        capacity_counter = 0;
+        out << "c" << counter << ": ";
+        for (int i = 0; i < vm_size; i++) {
+            if (capacity_counter == 0) {
+                out << vm_loads[i] << " a_" << u << "_" << i;
+            } else {
+                out << " + " << vm_loads[i] << " a_" << u << "_" << i;
+            }
+            capacity_counter++;
+        }
+        out << " <= " << pm_caps[u] << endl;
+        counter++;
+    }
+    int guarantee_counter = 0;
+    for (int i = 0; i < vm_size; i++) {
+        out << "c" << counter << ": ";
+        guarantee_counter = 0;
+        for (int u = 0; u < pm_size; u++ ) {
+            if (guarantee_counter == 0) {
+                out << "a_" << u << "_" << i;
+            } else {
+                out << " + " << "a_" << u << "_" << i;
+            }
+            guarantee_counter++;
+        }
+        out << " = 1" << endl;
+        counter++;
+    }
+    out << "Bounds" << endl;
+    int bound_counter = 0;
+    for (int u = 0; u < pm_size; u++) {
+        for (int v = 0; v < pm_size; v++) {
+            for (int i = 0; i < vm_size; i++) {
+                for (int j = 0; j < vm_size; j++) {
+                    out << "0 <= o_" << bound_counter << " <= 1" << endl;
+                    bound_counter++;
+                }
+            }
+        }
+    }
+    for (int u = 0; u < pm_size; u++) {
+        for (int i = 0; i < vm_size; i++) {
+            out << "0 <= a_" << u << "_" << i << " <= 1" << endl;
+        }
     }
     out << "End" << endl;
     out.close();
